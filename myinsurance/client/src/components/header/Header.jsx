7a98e77005ae7175e4c5d4bcff9f42c62ada497d
Arun Pandey <128284476+arunpandeyg@users.noreply.github.com>
2026-08-02 20:19:01 +0530

    Add files via upload

---
+++ myinsurance/client/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./header.css";
+
+
+const Header = () => {
+  return (
+    <header>
+      <div className="mainHead">
+        
+     
+    </div>
+    </header>
+    
+  )
+}
+
+export default Header;
---
+++ myinsurance/client/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+
+
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      
+    </section>
+  );
+};
+
+export default Home;
